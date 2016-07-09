574159c0d9b0460476c0989bcd693fef6cb31d19
Haggi <haggiderzweite@googlemail.com>
2016-07-09 14:39:17 +0200

    Added Caliburn Micro and Castle Windsow references, a small bootstrapper and an example window

---
+++ MusicPlayer/MusicPlayer/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Caliburn.Micro;
+using MusicPlayer.Views.MainWindow;
+
+namespace MusicPlayer
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        // Initializes caliburn micro and services
+
+
+        // this is a very barebones bootstrapper right now, and it will later include a lot more (fancy) stuff  
+
+        public Bootstrapper()
+        {
+            base.Initialize();
+        }
+
+
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+
+            // Creating a new window
+            var windowManager = new WindowManager();
+            var settings = new WindowSettings(500, 300, SizeToContent.Manual);
+
+            windowManager.ShowWindow(new MainWindowViewModel(), null, settings.Get());
+        }
+    }
+}
---
+++ MusicPlayer/MusicPlayer/Views/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+using MusicPlayer.Extensions;
+
+namespace MusicPlayer.Views.MainWindow
+{
+    public class MainWindowViewModel : Screen
+    {
+        private string _textBody;
+        private int _counter;
+        private string _textBox;
+
+        public string TextBody
+        {
+            get { return this._textBody; }
+            set
+            {
+                _textBody = value;
+                NotifyOfPropertyChange(() => TextBody);
+            }
+        }
+
+        public string TextBox
+        {
+            get { return this._textBox; }
+            set
+            {
+                _textBox = value;
+                NotifyOfPropertyChange(() => TextBox);
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            this.DisplayName = "The best Music Player ever!";
+            this.TextBody = "textbody";
+        }
+
+        public void ChangeMessage(int parameter)
+        {
+            if (this.TextBox.IsNumber())
+                this.TextBody = $"test{_counter += Convert.ToInt32(TextBox)}";
+
+        }
+
+    }
+}
