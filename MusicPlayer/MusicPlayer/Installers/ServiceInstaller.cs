bd32dfc7b0d9453c78438e3cfbf6da3f9bfd448f
Haggi <haggiderzweite@googlemail.com>
2016-07-10 15:08:49 +0200

    Renamed WindsorInstaller > SerivceInstaller, added basic AudioService and a testView

--- MusicPlayer/MusicPlayer/Installers/ServiceInstaller.cs
+++ MusicPlayer/MusicPlayer/Installers/ServiceInstaller.cs
@@ -5,15 +5,19 @@ using System.Text;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
+using MusicPlayer.Services.AudioService;
 using MusicPlayer.Services.Test;
 
 namespace MusicPlayer.Installers
 {
-    public class WindsorInstaller : IWindsorInstaller
+    public class ServiceInstaller : IWindsorInstaller
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            container.Register(Component.For<ITestService>().ImplementedBy<TestService>().LifestyleSingleton());
+            container.Register(
+                Component.For<ITestService>().ImplementedBy<TestService>().LifestyleSingleton(),
+                Component.For<IAudioService>().ImplementedBy<AudioService>().LifestyleSingleton()
+                );
 
         }
     }
---
+++ MusicPlayer/MusicPlayer/Services/AudioService/AudioService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Windows.Media;
+
+namespace MusicPlayer.Services.AudioService
+{
+    public class AudioService : IAudioService
+    {
+        private MediaPlayer _mediaPlayer;
+
+        public AudioService()
+        {
+            _mediaPlayer = new MediaPlayer();
+        }
+
+        public void  PlaySystemSound()
+        {
+            SystemSounds.Asterisk.Play();
+        }
+
+        public void PlayAudioFile(Uri uri)
+        {
+            _mediaPlayer.Open(uri);
+            _mediaPlayer.Play();
+        }
+    }
+}
