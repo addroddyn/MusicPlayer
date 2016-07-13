0f8e680d27cc781ff26f7f1fcaceebbc7c441288
Zack Collins PC <zacharyacol@gmail.com>
2016-07-12 20:00:29 -0500

    Added more methods to changing and retrieving mp3 tags

--- MusicPlayer/MusicPlayer/Services/MetaDataService/IMetaDataService.cs
+++ MusicPlayer/MusicPlayer/Services/MetaDataService/IMetaDataService.cs
@@ -5,5 +5,21 @@ namespace MusicPlayer.Services.MetaDataService
     public interface IMetaDataService
     {
         void SongTagLib(Uri path);
-    }
+
+        string getArtist();
+
+        void setArtist(string artist);
+
+        string getSongName();
+
+        void setSongTitle(string song);
+
+        string getAlbumTitle();
+
+        void setAlbumTitle(string album);
+
+        uint getYear();
+
+        void setYear(uint year);
+        
 }
--- MusicPlayer/MusicPlayer/Services/MetaDataService/MetaDataService.cs
+++ MusicPlayer/MusicPlayer/Services/MetaDataService/MetaDataService.cs
@@ -6,16 +6,61 @@ namespace MusicPlayer.Services.MetaDataService
     public class MetaDataService : IMetaDataService
     {
         private Uri _path;
-
+        private string _artist;
+        private string _songName;
+        private string _albumTitle;
+        private uint _year;
+        TagLib.File tagFile;
+        public MetaDataService()
+        {
+            
+        }
+        public MetaDataService(Uri path)
+        {
+            _path = path;
+            SongTagLib(_path);
+            TagLib.File tagFile = TagLib.File.Create(_path.ToString());
+        }
         public void SongTagLib(Uri path)
         {
             _path = path;
-            TagLib.File tagFile = TagLib.File.Create(_path.ToString());
-            string _artist = tagFile.Tag.FirstArtist;
+            string _artist = tagFile.Tag.AlbumArtists[0];
             string _songName = tagFile.Tag.Title;
             string _albumTitle = tagFile.Tag.Album;
             uint _year = tagFile.Tag.Year;
-
         }
+        public string getArtist()
+        {
+            return _artist;
+        }
+        public void setArtist(string artist)
+        {
+            tagFile.Tag.AlbumArtists[0] = artist;
+        }
+        public string getSongName()
+        {
+            return _songName;
+        }
+        public void setSongTitle(string song)
+        {
+            tagFile.Tag.Title = song;
+        }
+        public string getAlbumTitle()
+        {
+            return _albumTitle;
+        }
+        public void setAlbumTitle(string album)
+        {
+            tagFile.Tag.Album = album;
+        }
+        public uint getYear()
+        {
+            return _year;
+        }
+        public void setYear(uint year)
+        {
+            tagFile.Tag.Year = year;
+        }
+
     }
 }
